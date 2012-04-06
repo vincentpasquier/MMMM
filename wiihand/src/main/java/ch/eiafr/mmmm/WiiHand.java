5fdfb92ee5657fae8599d25311b53e96c7ed0add
Djanick Jaymeliss <yannick.jemmely@gmail.com>
2012-04-06 23:33:17 +0200

    add wiihead connection and test

--- wiihand/src/main/java/ch/eiafr/mmmm/WiiHand.java
+++ wiihand/src/main/java/ch/eiafr/mmmm/WiiHand.java
@@ -1,7 +1,42 @@
 package ch.eiafr.mmmm;
 
+import motej.Mote;
+import motej.event.IrCameraEvent;
+import motej.event.IrCameraListener;
+
 public class WiiHand {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		Mote mote = new Mote("E84ECECD5999");
+		mote.addIrCameraListener(new IrCameraListener() {
+
+			@Override
+			public void irImageChanged(IrCameraEvent arg0) {
+				System.out.println(arg0.toString());
+			}
+		});
+		//
+		// final ArrayList<Mote> motes = new ArrayList<Mote>();
+		// MoteFinderListener listener = new MoteFinderListener() {
+		//
+		// public void moteFound(Mote mote) {
+		// System.out.println("Found mote: " + mote.getBluetoothAddress());
+		// mote.rumble(2000l);
+		// motes.add(mote);
+		// }
+		//
+		// };
+		//
+		// MoteFinder finder = MoteFinder.getMoteFinder();
+		// finder.addMoteFinderListener(listener);
+		//
+		// finder.startDiscovery();
+		// try {
+		// Thread.sleep(2000);
+		// } catch (InterruptedException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		// //finder.stopDiscovery();
 	}
 }
